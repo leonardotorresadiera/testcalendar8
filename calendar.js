8bb68667fea801eb0ddb6911d71a0199044f6b98
Leonardo Torres Ochoa <leonardotorres@adiera.com>
2018-04-22 14:15:07 -0500

    show calendar

--- calendar.js
+++ calendar.js
@@ -10,25 +10,57 @@ var CalendarTest = function() {
         $("#inputDateStart").datepicker("update", startDate);
         $("#inputNumberDays").inputmask({"alias": "integer","autoGroup": false,"autoUnmask": true,"noshift": true});
     };
-    var createMonth =  function() {
+    var getNewDateEnd = function() {
         var numberOfDays = $("#inputNumberDays").val();
+        var d1 = moment($("#inputDateStart").val(),"MM/DD/YYYY");
+        var tmp1 = d1.add(numberOfDays, 'days');
+        var dateEnd = tmp1.toDate();
+        console.log("dateEnd:"+dateEnd);
+    };
+    var getDayEndMonth = function() {
+        var d1 = moment($("#inputDateStart").val(),"MM/DD/YYYY");
+        var endDaysMonth = d1.endOf('month').format('DD');
+        console.log("endDaysMonth:"+endDaysMonth);
+        return endDaysMonth;
+    };
+    var createMonth =  function() {
         var dateInput = moment($("#inputDateStart").val(),"MM/DD/YYYY");
+        var endDaysMonth = getDayEndMonth();
         var monthName = dateInput.format("MMMM YYYY");
         var dayOfMonth = dateInput.format("D"); 
         var dayOfWeek = dateInput.format("d");
-        console.log("dayOfWeek:"+dayOfWeek);
+
         var html = '<br><center>'+monthName+'</center><div class="row"><div class="col-sm-6" style="background-color:azure">';
         var i,j=0;
         html+='<div class="row"><div class="col-sm-1">S</div><div class="col-sm-1">M</div><div class="col-sm-1">T</div><div class="col-sm-1">W</div><div class="col-sm-1">T</div><div class="col-sm-1">F</div><div class="col-sm-1">S</div></div>';
 
+        var flag = true;
         for (i = 0; i < 4; i++) { 
             html+='<div class="row">';
             for (j = 0; j< 7; j++) { 
-                html+='<div class="col-sm-1">'+i+j+'</div>';
+                if (flag==true) {
+                    if (j>=dayOfWeek) {
+                        html+='<div class="col-sm-1">'+dayOfMonth+'</div>';        
+                        dayOfMonth++;
+                        flag=false;
+                    }
+                    else {
+                        html+='<div class="col-sm-1">&nbsp;</div>';
+                    }
+                } 
+                else {
+                    if (dayOfMonth>endDaysMonth) {
+                        html+='<div class="col-sm-1">&nbsp;</div>';        
+                    }
+                    else {
+                        html+='<div class="col-sm-1">'+dayOfMonth+'</div>';        
+                    }
+                    dayOfMonth++;
+                }
             }    
             html+='</div>';
         }            
-        
+
         html +="</div></div>";
         return html;
     };

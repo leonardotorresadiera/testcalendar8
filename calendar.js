cbaf836b4d9665a4cbd273525842a049e01fa2b1
Leonardo Torres Ochoa <leonardotorres@adiera.com>
2018-04-22 12:57:19 -0500

    create input form

--- calendar.js
+++ calendar.js
@@ -1,6 +1,13 @@
 var CalendarTest = function() {
     "use strict";
     var datePickerHandler = function() {
+        $('.datepickerBoot').datepicker({
+            autoclose: true,
+            todayHighlight: true,
+            todayBtn: true,
+        });
+        var startDate = new Date();
+        $("#inputDateStart").datepicker("update", startDate);
     };
     return {
         init: function() {
